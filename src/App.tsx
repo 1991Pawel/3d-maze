a251446e185d5087b5d8aa20ca3bd28a5b2f49df
Paweł Leśnik <pawel.lesnik@tdy.pl>
2026-09-06 13:07:31 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./components/Experience";
+import { KeyboardControls } from "@react-three/drei";
 
 export default function App() {
   return (
-    <Canvas
-      shadows
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [2.5, 4, 6],
-      }}
+    <KeyboardControls
+      map={[
+        { name: "forward", keys: ["ArrowUp", "KeyW"] },
+        { name: "backward", keys: ["ArrowDown", "KeyS"] },
+        { name: "leftward", keys: ["ArrowLeft", "KeyA"] },
+        { name: "rightward", keys: ["ArrowRight", "KeyD"] },
+        { name: "space", keys: ["Space"] },
+      ]}
     >
-      <Experience />
-    </Canvas>
+      <Canvas
+        shadows
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [2.5, 4, 6],
+        }}
+      >
+        <Experience />
+      </Canvas>
+    </KeyboardControls>
   );
 }
---
+++ src/components/Player.jsx
@@ -0,0 +1,54 @@
+import { RigidBody } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import { useKeyboardControls } from "@react-three/drei";
+import { useRef } from "react";
+export default function Player() {
+  const [subscribeKeys, getKeys] = useKeyboardControls();
+  const body = useRef();
+  useFrame((state, delta) => {
+    const { forward, backward, leftward, rightward, space } = getKeys();
+
+    const impulse = { x: 0, y: 0, z: 0 };
+    const torque = { x: 0, y: 0, z: 0 };
+
+    const impulseStrength = 1 * delta;
+    const torqueStrength = 1 * delta;
+
+    if (forward) {
+      impulse.z -= impulseStrength;
+      torque.x -= torqueStrength;
+
+      console.log("forward");
+    }
+    if (backward) {
+      console.log("backward");
+    }
+    if (leftward) {
+      console.log("leftward");
+    }
+    if (rightward) {
+      console.log("rightward");
+    }
+    if (space) {
+      console.log("space");
+    }
+
+    body.current.applyImpulse(impulse);
+    body.current.applyTorqueImpulse(torque);
+  });
+  return (
+    <RigidBody
+      ref={body}
+      canSleep={false}
+      colliders="ball"
+      restitution={0.2}
+      friction={1}
+      position={[0, 1, 0]}
+    >
+      <mesh castShadow>
+        <icosahedronGeometry args={[0.3, 1]} />
+        <meshStandardMaterial color="mediumpurple" flatShading />
+      </mesh>
+    </RigidBody>
+  );
+}
